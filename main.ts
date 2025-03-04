3c2f210f428a893efd10308ff3618f519157e212
Stuart Sackler <stuart@sackler.ca>
2025-03-03 21:33:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+let yVal = 0
+let xVal = 0
+basic.forever(function () {
+    xVal = pins.analogReadPin(AnalogPin.P0)
+    yVal = pins.analogReadPin(AnalogPin.P1)
+    if (xVal > 600) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (xVal < 400) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (yVal < 400) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (yVal > 600) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
